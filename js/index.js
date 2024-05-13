6e6a0adff46c5d95368cfac47d5cc00fdd9033f5
MauricioUno <mauriciouno37@gmail.com>
2024-05-13 20:24:05 -0300

    Inicio

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {leer, escribir, limpiar} from "./localStorage-delay.js";
+document.addEventListener("DOMContentLoaded", onInit);
+
+const elementos = [];
+const formulario = document.getElementById("form-item");;
+
+function onInit() {
+}
